e82084f0df9cacdb892581c96947366ba08bdc4e
ChrisCastroV08 <chriscv1608@gmail.com>
2026-02-03 17:40:54 -0600

    Fail state

--- Examen2/app/src/test/java/com/example/examen2/CalculadoraTest.java
+++ Examen2/app/src/test/java/com/example/examen2/CalculadoraTest.java
@@ -106,6 +106,6 @@ public class CalculadoraTest {
     @Test
     public void testEncuentraElementoPresente() {
         List<Integer> lista = Arrays.asList(1, 2, 3, 4, 5);
-        assertTrue(Calculadora.Encuentra(lista, 3));
+        assertTrue(Calculadora.Encuentra(lista, "a"));
     }
 }
